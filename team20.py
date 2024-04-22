1a99a8c69efef5e39078b2cfc024cb31e8347f1f
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2024-04-22 13:35:04 +0530

    third commit

--- team20.py
+++ team20.py
@@ -2,20 +2,38 @@ import hmac
 import hashlib
 import string
 import random
-import datetime
+import time
 
 acc_number = 3001
+trans_id = 5001
 
 
-class Register:
+class Account:
     def __init__(self):
         self.userInfo = {}
         self.secretKeys = []
+        self.transactions = {}
 
     def generateSecretKey(self):
         keysize = 5
-        res = "".join(random.choices(string.ascii_uppercase + string.digits, k=keysize))
+        res = "".join(random.choices(string.ascii_uppercase, k=keysize))
         return res
+
+    def viewUser(self, accno):
+        print(f"Transactions done by Account No. {accno}:\n")
+        for i in self.transactions[accno]:
+            if i[1] == "D":
+                print(f"{i[0]}: Deposited amount: {i[2]}")
+            elif i[1] == "W":
+                print(f"{i[0]}: Widrawal amount: {i[2]}")
+            elif i[1] == "Tin":
+                print(
+                    f"{i[0]}: Transfered from (credited) AccountNo. {i[2]} amount {i[3]}"
+                )
+            elif i[1] == "Tout":
+                print(
+                    f"{i[0]}: Transfered to (debited) AccountNo. {i[2]} amount {i[3]}"
+                )
 
 
 class Auth:
@@ -28,26 +46,137 @@ class Auth:
         rbit = random.randint(0, 1)
         return rbit
 
-    def create_response(secret_key, rbit, challenge, amt):
+    def create_response(self, secret_key, rbit, challenge, amt):
         hash_func = hashlib.sha256
         msg = str(rbit + challenge + amt)
         hmsg = hmac.new(secret_key.encode(), msg.encode(), hash_func)
         hmac_value = hmsg.hexdigest()
         return [amt, hmac_value]
 
-    def verify_response(secret_key, rbit, challenge, hmac_res, amt):
+    def verify_response(self, secret_key, rbit, challenge, hmac_res, amt):
         hash_func = hashlib.sha256
         msg = str(rbit + challenge + amt)
         hmsg = hmac.new(secret_key.encode(), msg.encode(), hash_func)
         hmac_value = hmsg.hexdigest()
         if hmac_value == hmac_res[1] and amt == hmac_res[0]:
-            return True
+            return [True, hmac_value]
         else:
-            return False
+            return [False, hmac_value]
 
 
-reg = Register()
+class Blockchain:
+    def __init__(self):
+        self.verifiedTransac = {}
+        self.pendingTranac = {}
+        self.blockchainedTransac = {}
+        self.chain = []
+        self.blockNotMined = []
+
+    def initiategenesisblock(self, timestmp):
+        blk = {
+            "index": len(self.chain) + 1,
+            "merkleroot": "0",
+            "trans_ID": "0",
+            "timestamp": timestmp,
+            "blockproducer": "0",
+            "blockminer": "0",
+            "prevhash": "0",
+            "hash": "0",
+        }
+        self.chain.append(blk)
+        blk_msg = f"""
+[
+    index: {blk['index']},
+    merkleroot: {blk['merkleroot']},
+    transaction_IDs: nil,
+    timestamp: {blk['timestamp']},
+    blockproducer: 0
+    blockminer: 0
+    prevhash: {blk['prevhash']},
+    hash: {blk['hash']}
+]
+"""
+        print("Genesis Block:\n")
+        print(blk_msg)
+
+    def addUnverifiedTransaction(self, timestamp, transaction, tid):
+        msg = f"""[
+            transaction_id: {tid}
+            timestamp: {timestamp},
+            transaction: {transaction}
+]"""
+        self.pendingTranac[tid] = msg
+        return msg
+
+    def verifytransaction(self, transac_id, accno, skey, auth, acc):
+        chal = auth.generate_challenge()
+        print(f"\nRandom challenge: {chal}")
+        rbit = auth.generate_randomBit()
+        print(f"Random bit: {rbit}")
+        msghmac = auth.create_response(skey, rbit, chal, transac_id)
+        print(f"Response HMAC digest: {msghmac[1]}")
+        verif = auth.verify_response(
+            acc.userInfo[accno]["secretkey"],
+            rbit,
+            chal,
+            msghmac,
+            msghmac[0],
+        )
+        print(f"Calculated HMAC digest: {verif[1]}")
+        if verif[0]:
+            print("Calculated HMAC matched with received response!")
+            self.verifiedTransac[transac_id] = self.pendingTranac[transac_id]
+            del self.pendingTranac[transac_id]
+            print(f"\nTransactionID: {transac_id} is verified!\n")
+        else:
+            print("Calculated HMAC did not match with received response")
+            print("Authentication failed\n")
+
+    def merkletree(self, t1, t2, t3):
+        hashed = []
+        t1_msg = self.verifiedTransac[t1]
+        t2_msg = self.verifiedTransac[t2]
+        t3_msg = self.verifiedTransac[t3]
+        hashed.append(str(hashlib.sha256(t1_msg.encode()).hexdigest()))
+        hashed.append(str(hashlib.sha256(t2_msg.encode()).hexdigest()))
+        hashed.append(str(hashlib.sha256(t3_msg.encode()).hexdigest()))
+        hash12_msg = hashed[0] + hashed[1]
+        hash12 = str(hashlib.sha256(hash12_msg.encode()).hexdigest())
+        hash33_msg = hashed[2] + hashed[2]
+        hash33 = str(hashlib.sha256(hash33_msg.encode()).hexdigest())
+        hash1233_msg = hash12 + hash33
+        hash1233 = str(hashlib.sha256(hash1233_msg.encode()).hexdigest())
+        return hash1233
+
+    def createBlock(self, blkproducer):
+        blk_dict = {}
+        blk_dict["index"] = len(self.chain) + 1
+        tids = []
+        ct = 0
+        for i in self.verifiedTransac:
+            if ct >= 3:
+                break
+            else:
+                ct += 1
+                tids.append(i)
+        tidstr = ""
+        for i in tids:
+            tidstr += str(i)
+            tidstr += " "
+        blk_dict["trans_ID"] = tidstr
+        merkleroothash = self.merkletree(tids[0], tids[1], tids[2])
+        blk_dict["merkleroot"] = merkleroothash
+        blk_dict["blockproducer"] = str(blkproducer)
+        self.blockNotMined.append(blk_dict)
+
+    # def verifyTransaction(self,trans):
+
+
+acc = Account()
 auth = Auth()
+blk = Blockchain()
+
+blk.initiategenesisblock(time.time())
 
 print("\t\t******************************************************************")
 print("\t\t\t\t\tNET BANKING SYSTEM")
@@ -65,9 +194,11 @@ while True:
         2 : Deposit Amount
         3 : Withdraw Amount
         4 : Transfer Amount
-        5 : Balance Enquiry
-        6 : View Transactions
-        7 : All Account holder list
+        5 : Verify Transaction(s)
+        6 : Create / Mine Block
+        7 : View Blockchain / Transactions
+        8 : View User Transactions
+        9 : View User Details 
         0 : Exit
     
     """
@@ -76,48 +207,240 @@ while True:
     if choice == "1":
         uname = input("\nEnter the account holder name : ")
         balance = int(input("Enter The initial deposit amount : "))
-        tpin = int(input("Setup pin number (integer format): "))
+        # tpin = int(input("Setup pin number (integer format): "))
         print("\n\nAccount Successfully registered!")
         accno = acc_number
         acc_number += 1
-        scode = reg.generateSecretKey()
-        while scode in reg.secretKeys:
-            scode = reg.generateSecretKey()
+        scode = acc.generateSecretKey()
+        while scode in acc.secretKeys:
+            scode = acc.generateSecretKey()
         print(
-            f"Account number: {accno}\nSecret code (never share with others): {scode}\nCurrent Balance: {balance}"
+            f"Account number: {accno}\nSecret key (never share with others): {scode}\nCurrent Balance: {balance}"
         )
-        reg.userInfo[acc_number] = {
+        acc.userInfo[accno] = {
             "name": uname,
             "balance": balance,
-            "pin": tpin,
             "accountno": accno,
-            "secretcode": scode,
+            "secretkey": scode,
         }
-        reg.secretKeys.append(scode)
+        acc.transactions[accno] = []
+        acc.secretKeys.append(scode)
+
     elif choice == "0":
         break
     elif choice == "2":
         print("\nDEPOSIT AMOUNT")
         login_account = int(input("Enter Account Number: "))
-        if login_account in reg.userInfo:
-            login_pin = int(input("Enter Pin Number: "))
-            if login_pin == reg.userInfo[login_account]:
-                deposit_amt = int(input("Enter Deposit Amount: "))
-                chal = auth.generate_challenge()
-                rbit = auth.generate_randomBit()
-                msghmac = auth.create_response(rbit, chal, deposit_amt)
-                if auth.verify_response(rbit, chal, msghmac, msghmac[0]):
-                    reg.userInfo[login_account]["balance"] += deposit_amt
-                    print(
-                        # f"Account No. {login_account} credited with {deposit_amt}\nCurrent Balance: f{reg.userInfo[login_account]}\n"
-                        f"Account No. {login_account} credited with {deposit_amt}\n"
-                    )
-                else:
-                    print("Authentication failed\n")
+        if login_account in acc.userInfo:
+            deposit_amt = int(input("Enter Deposit Amount: "))
+            skey = input("Enter secret key: ")
+            chal = auth.generate_challenge()
+            print(f"\nRandom challenge: {chal}")
+            rbit = auth.generate_randomBit()
+            print(f"Random bit: {rbit}")
+            msghmac = auth.create_response(skey, rbit, chal, deposit_amt)
+            print(f"Response HMAC digest: {msghmac[1]}")
+            verif = auth.verify_response(
+                acc.userInfo[login_account]["secretkey"],
+                rbit,
+                chal,
+                msghmac,
+                msghmac[0],
+            )
+            print(f"Calculated HMAC digest: {verif[1]}")
+            if verif[0]:
+                print("Calculated HMAC matched with received response!")
+                acc.userInfo[login_account]["balance"] += deposit_amt
+                acc.transactions[login_account].append([trans_id, "D", deposit_amt])
+                trans_mempool = blk.addUnverifiedTransaction(
+                    time.time(),
+                    f"AccountNo. {login_account} Deposited amount {deposit_amt}",
+                    trans_id,
+                )
+                trans_id += 1
+                print(
+                    f"\nAccount No. {login_account} credited with {deposit_amt}\nCurrent Balance: {acc.userInfo[login_account]['balance']}\n"
+                )
+                print("\nTransaction added to Mempool: ")
+                print(trans_mempool)
             else:
-                print("Invalid Pin\n")
+                print("Calculated HMAC did not match with received response")
+                print("Authentication failed\n")
         else:
             print("Invalid Account Number\n")
 
+    elif choice == "3":
+        print("\nWITHDRAW AMOUNT")
+        login_account = int(input("Enter Account Number: "))
+        if login_account in acc.userInfo:
+            wd_amt = int(input("Enter Withdrawal amount: "))
+            if acc.userInfo[login_account]["balance"] > wd_amt:
+                skey = input("Enter secret key: ")
+                chal = auth.generate_challenge()
+                print(f"\nRandom challenge: {chal}")
+                rbit = auth.generate_randomBit()
+                print(f"Random bit: {rbit}")
+                msghmac = auth.create_response(skey, rbit, chal, wd_amt)
+                print(f"Response HMAC digest: {msghmac[1]}")
+                verif = auth.verify_response(
+                    acc.userInfo[login_account]["secretkey"],
+                    rbit,
+                    chal,
+                    msghmac,
+                    msghmac[0],
+                )
+                print(f"Calculated HMAC digest: {verif[1]}")
+                if verif[0]:
+                    print("Calculated HMAC matched with received response!")
+                    acc.userInfo[login_account]["balance"] -= wd_amt
+                    acc.transactions[login_account].append([trans_id, "W", wd_amt])
+                    trans_mempool = blk.addUnverifiedTransaction(
+                        time.time(),
+                        f"AccountNo. {login_account} Withdrawal amount {wd_amt}",
+                        trans_id,
+                    )
+                    trans_id += 1
+                    print(
+                        f"\nAccount No. {login_account} debited with {wd_amt}\nCurrent Balance: {acc.userInfo[login_account]['balance']}\n"
+                    )
+                    print("\nTransaction added to Mempool: ")
+                    print(trans_mempool)
+                else:
+                    print("Calculated HMAC did not match with received response")
+                    print("Authentication failed\n")
+            else:
+                print("Insufficient Balance\n")
+        else:
+            print("Invalid Account Number\n")
+    elif choice == "4":
+        print("\nTRANSFER AMOUNT")
+        from_acc = int(input("Enter Source Account Number: "))
+        if from_acc in acc.userInfo:
+            to_acc = int(input("Enter Destination Account Number: "))
+            if to_acc in acc.userInfo:
+                tranf_amt = int(input("Enter amount to be transfered: "))
+                if acc.userInfo[from_acc]["balance"] > tranf_amt:
+                    skey = input("Enter Secret Key: ")
+                    chal = auth.generate_challenge()
+                    print(f"\nRandom challenge: {chal}")
+                    rbit = auth.generate_randomBit()
+                    print(f"Random bit: {rbit}")
+                    msghmac = auth.create_response(skey, rbit, chal, tranf_amt)
+                    print(f"Response HMAC digest: {msghmac[1]}")
+                    verif = auth.verify_response(
+                        acc.userInfo[from_acc]["secretkey"],
+                        rbit,
+                        chal,
+                        msghmac,
+                        msghmac[0],
+                    )
+                    print(f"Calculated HMAC digest: {verif[1]}")
+                    if verif[0]:
+                        print("Calculated HMAC matched with received response!")
+                        acc.userInfo[from_acc]["balance"] -= tranf_amt
+                        acc.userInfo[to_acc]["balance"] += tranf_amt
+                        acc.transactions[from_acc].append(
+                            [trans_id, "Tout", to_acc, tranf_amt]
+                        )
+                        acc.transactions[to_acc].append(
+                            [trans_id, "Tin", from_acc, tranf_amt]
+                        )
+                        trans_mempool = blk.addUnverifiedTransaction(
+                            time.time(),
+                            f"AccountNo. {from_acc} transfered to AccountNo. {to_acc} amount {tranf_amt}",
+                            trans_id,
+                        )
+                        trans_id += 1
+                        print(
+                            f"\nAccount No. {from_acc} debited with {tranf_amt}\nCurrent Balance: {acc.userInfo[login_account]['balance']}\n"
+                        )
+                        print("\nTransaction added to Mempool: ")
+                        print(trans_mempool)
+                    else:
+                        print("Calculated HMAC did not match with received response")
+                        print("Authentication failed\n")
+                else:
+                    print("Insufficient Balance\n")
+            else:
+                print("Account not found\n")
+        else:
+            print("Invalid Account Number\n")
+    elif choice == "5":
+        print("VERIFY TRANSACTION(S)")
+        login_account = int(input("Enter Account Number: "))
+        if login_account in acc.userInfo:
+            ch = input(
+                "1: Verify a Transaction\n2: Verify all Transactions\nSelect Action: "
+            )
+            if ch == "1":
+                print("Unverified Transaction ID List: ")
+                for i in blk.pendingTranac:
+                    print(i)
+                transac_id = int(input("Enter Transaction ID: "))
+                if i in blk.pendingTranac:
+                    skey = input("Enter your secret key: ")
+                    blk.verifytransaction(transac_id, login_account, skey, auth, acc)
+                else:
+                    print("Invalid TransactionID\n")
+            elif ch == "2":
+                print("Unverified TransactionID list: ")
+                unverif_transacID = []
+                for i in blk.pendingTranac:
+                    print(i)
+                    unverif_transacID.append(i)
+                skey = input("Enter your secret key: ")
+                for i in unverif_transacID:
+                    blk.verifytransaction(i, login_account, skey, auth, acc)
+            else:
+                print("Invalid action\n")
+        else:
+            print("Invalid Account Number\n")
+    elif choice == "6":
+        print("\nCREATE/MINE BLOCK")
+        if len(blk.verifiedTransac) < 3:
+            print("\nMinimum of 3 transactions needed to create a block\n")
+        else:
+            ch = input("\n1: Create Block\n2: Mine Block\nSelect Action: ")
+            if ch == "1":
+                login_account = random.randint(3001, acc_number)
+                print(f"\nAccountNo {login_account} is creating a block\n")
+
+    elif choice == "7":
+        ch = input(
+            "\n1: View Blockchain\n2: View Transactions (verified/unverified)\nSelect action: "
+        )
+        if ch == "2":
+            print("\nVerified TransactionID List: ")
+            for i in blk.verifiedTransac:
+                print(i)
+            if len(blk.verifiedTransac) == 0:
+                print("NA\n")
+            print("\nUnverified TransactionID List: ")
+            for i in blk.pendingTranac:
+                print(i)
+            if len(blk.pendingTranac) == 0:
+                print("NA\n")
+
+    elif choice == "8":
+        print("\nVIEW USER TRANSACTIONS")
+        print("AccountNo. list: ")
+        for i in acc.userInfo:
+            print(i)
+        login_account = int(input("Enter Account Number: "))
+        if login_account in acc.userInfo:
+            acc.viewUser(login_account)
+        else:
+            print("Invalid Account Number\n")
+    elif choice == "9":
+        print("\nVIEW USER DETAILS")
+        print("AccountNo. list: ")
+        for i in acc.userInfo:
+            print(i)
+        login_account = int(input("Enter Account Number: "))
+        if login_account in acc.userInfo:
+            nam = acc.userInfo[login_account]["name"]
+            bal = acc.userInfo[login_account]["balance"]
+            print(f"Name: {nam}\nBalance: {bal}")
+            acc.viewUser(login_account)
     else:
         print("\nInvalid choice\n\n")
